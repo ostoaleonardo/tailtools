db6354f62e28ad7d877188c3557e49c5172ddc4b
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-01-26 18:00:39 -0600

    Show a check when a color is copied on random palettes page

--- src/components/RandomPalette/HoverColor.tsx
+++ src/components/RandomPalette/HoverColor.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Button, Tooltip } from '@nextui-org/react'
 import { Icons } from '..'
 
@@ -8,16 +9,24 @@ interface Color {
 }
 
 export function HoverColor({ color }: { color: Color }) {
+    const [isCopied, setIsCopied] = useState(false)
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(color.hex)
+        setIsCopied(true)
+        setTimeout(() => setIsCopied(false), 1000)
+    }
+
     return (
         <div className='absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center opacity-0 hover:opacity-100 transition-opacity gap-2'>
             <Tooltip content='Copy color' placement='left' delay={700}>
                 <Button
                     isIconOnly
                     variant='light'
+                    onPress={handleCopy}
                     style={{ color: color.contrast }}
-                    onPress={() => navigator.clipboard.writeText(color.hex)}
                 >
-                    <Icons.Copy />
+                    {isCopied ? <Icons.Check /> : <Icons.Copy />}
                 </Button>
             </Tooltip>
         </div>
