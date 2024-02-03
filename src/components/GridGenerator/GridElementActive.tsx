ec511eec6c17593f806b9abc5226de627a66559d
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-02-03 14:48:31 -0600

    Formatting code

--- src/components/GridGenerator/GridElementActive.tsx
+++ src/components/GridGenerator/GridElementActive.tsx
@@ -1,4 +1,4 @@
-import { Icons } from ".."
+import { Icons } from '..'
 
 interface Props {
     item: number | string
--- src/components/UI/Navbar/NavHoverRoute.tsx
+++ src/components/UI/Navbar/NavHoverRoute.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from 'react-router-dom'
 
 interface Props {
     route: { path: string, name: string, description: string }
