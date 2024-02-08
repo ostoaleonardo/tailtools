e492121a2c2c64c4a577fcfeab6fe306fa611016
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-02-07 20:45:39 -0600

    Add scroll shadow in gradient creator colors

--- src/pages/GradientCreator.tsx
+++ src/pages/GradientCreator.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { ColorStopsTabs, GradientCode, GradientCreatorHeader, GradientVisualizer, OtherColorsRow, PaletteRow } from '../components'
 import { INITIAL_GRADIENT, PALETTES, GRADIENT_CLASSES } from '../constants'
+import { ScrollShadow } from '@nextui-org/react'
 
 export function GradientCreator() {
     const [code, setCode] = useState('bg-gradient-to-tr from-slate-600 to-slate-400')
@@ -44,13 +45,18 @@ export function GradientCreator() {
                             colorStop={colorStop}
                             setColorStop={setColorStop}
                         />
-                        <div className='w-full h-80 border-2 border-black/10 dark:border-white/10 rounded-2xl overflow-y-auto'>
-                            <div className='w-full flex flex-col items-center justify-center gap-10 p-6'>
-                                {PALETTES.map((palette, index) => (
-                                    <PaletteRow key={index} palette={palette} setColors={setGradientValues} />
-                                ))}
-                                <OtherColorsRow colorStop={colorStop} setColors={setGradientValues} />
-                            </div>
+                        <div className='w-full h-80 border-2 border-black/10 dark:border-white/10 rounded-2xl'>
+                            <ScrollShadow
+                                hideScrollBar
+                                className='w-full h-full overflow-y-auto'
+                            >
+                                <div className='w-full flex flex-col items-center justify-center gap-10 p-6'>
+                                    {PALETTES.map((palette, index) => (
+                                        <PaletteRow key={index} palette={palette} setColors={setGradientValues} />
+                                    ))}
+                                    <OtherColorsRow colorStop={colorStop} setColors={setGradientValues} />
+                                </div>
+                            </ScrollShadow>
                         </div>
                         <GradientCode code={code} />
                     </div>
