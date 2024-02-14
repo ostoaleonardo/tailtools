4edefc9e4820178630405ad5fa84311174a2213c
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-02-13 19:32:15 -0600

    Update component preview

--- src/components/TailwindComponents/Components/Texts/TypeWriter.tsx
+++ src/components/TailwindComponents/Components/Texts/TypeWriter.tsx
@@ -1,6 +1,6 @@
 export function TypeWriter() {
     return (
-        <h2 className='relative text-4xl font-console before:absolute before:inset-0 before:bg-slate-200 before:dark:bg-zinc-900 before:animate-typewriter after:absolute after:inset-0 after:bg-black after:dark:bg-white after:w-0.5 after:animate-caret transition-all'>
+        <h2 className='relative text-4xl font-console before:absolute before:inset-0 before:bg-slate-200 before:dark:bg-titan-black before:animate-typewriter after:absolute after:inset-0 after:bg-black after:dark:bg-white after:w-0.5 after:animate-caret transition-all'>
             Type Writer
         </h2>
     )
