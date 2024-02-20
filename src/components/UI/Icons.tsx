013917701e3cfce07ad731e10b7ae7502ed5d045
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-02-19 19:11:48 -0600

    Icons code refactoring

--- src/components/UI/Icons.tsx
+++ src/components/UI/Icons.tsx
@@ -1,4 +1,4 @@
-export const Add = (props: any) => (
+const Add = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M17 15V8h-2v7H8v2h7v7h2v-7h7v-2z' />
     </svg>
@@ -10,67 +10,67 @@ const Search = (props: any) => (
     </svg>
 )
 
-export const Load = (props: any) => (
+const Load = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M11 1h1c6.075 0 11 4.925 11 11s-4.925 11-11 11S1 18.075 1 12v-1h2v1a9 9 0 1 0 9-9h-1z' />
     </svg>
 )
 
-export const Close = (props: any) => (
+const Close = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' fillRule='evenodd' d='m8 8.707l3.646 3.647l.708-.707L8.707 8l3.647-3.646l-.707-.708L8 7.293L4.354 3.646l-.707.708L7.293 8l-3.646 3.646l.707.708z' clipRule='evenodd' />
     </svg>
 )
 
-export const Check = (props: any) => (
+const Check = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 1024 1024' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M406.656 706.944L195.84 496.256a32 32 0 1 0-45.248 45.248l256 256l512-512a32 32 0 0 0-45.248-45.248L406.592 706.944z' />
     </svg>
 )
 
-export const Copy = (props: any) => (
+const Copy = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M9 9V6.2c0-1.12 0-1.68.218-2.108c.192-.377.497-.682.874-.874C10.52 3 11.08 3 12.2 3h5.6c1.12 0 1.68 0 2.108.218a2 2 0 0 1 .874.874C21 4.52 21 5.08 21 6.2v5.6c0 1.12 0 1.68-.218 2.108a2.002 2.002 0 0 1-.874.874C19.48 15 18.92 15 17.803 15H15M9 9H6.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C3 10.52 3 11.08 3 12.2v5.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h5.607c1.117 0 1.676 0 2.104-.218a2 2 0 0 0 .874-.874c.218-.428.218-.987.218-2.105V15M9 9h2.8c1.12 0 1.68 0 2.108.218a2 2 0 0 1 .874.874c.218.427.218.987.218 2.105V15' />
     </svg>
 )
 
-export const Export = (props: any) => (
+const Export = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 16 16' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M11.78 5.841a.75.75 0 0 1-1.06 0l-1.97-1.97v7.379a.75.75 0 0 1-1.5 0V3.871l-1.97 1.97a.75.75 0 0 1-1.06-1.06l3.25-3.25L8 1l.53.53l3.25 3.25a.75.75 0 0 1 0 1.061M2.5 9.75a.75.75 0 0 0-1.5 0V13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V9.75a.75.75 0 0 0-1.5 0V13a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5z' />
     </svg>
 )
 
-export const Heart = (props: any) => (
+const Heart = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M458.4 64.3C400.6 15.7 311.3 23 256 79.3C200.7 23 111.4 15.6 53.6 64.3C-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9c14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3m-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7c38.9-32.7 98.9-27.8 136.5 10.5l35 35.7l35-35.7c37.8-38.5 97.8-43.2 136.5-10.6c51.1 43.1 43.5 113.9 7.3 150.8' />
     </svg>
 )
 
-export const Random = (props: any) => (
+const Random = (props: any) => (
     <svg width='1.11em' height='1em' viewBox='0 0 520 472' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M70 365q74 0 118-57q0-4-5-7l-19-38q-13 27-38.5 43.5T70 323H21q-8 0-14.5 6.5T0 344t6.5 14.5T21 365zM442 9q-16-14-30 0q-15 15 0 30l27 28h-83q-73 0-117 57q0 3 4 7l19 38q13-27 38.5-43.5T356 109h83l-27 28q-15 15 0 30q6 6 15 6q7 0 15-6l64-64q13-15 0-30zm0 256q-16-14-30 0q-15 15 0 30l27 28h-83q-30 0-56-16.5T260 263l-23-47l-24-47l-10-19q-18-38-54-60.5T70 67H21q-8 0-14.5 6.5T0 88t6.5 14.5T21 109h49q64 0 96 60l24 47l23 47l11 19q20 38 55.5 60.5T358 365h84l-28 28q-15 15 0 30q6 6 15 6q8 0 15-6l64-64q13-15 0-30z' />
     </svg>
 )
 
-export const Background = (props: any) => (
+const Background = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='none' stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m4 8l4-4m6 0L4 14m0 6L20 4m0 6L10 20m10-4l-4 4' />
     </svg>
 )
 
-export const Text = (props: any) => (
+const Text = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M21 6v2H3V6zM3 18h9v-2H3zm0-5h18v-2H3z' />
     </svg>
 )
 
-export const Sun = (props: any) => (
+const Sun = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M256 104c-83.813 0-152 68.187-152 152s68.187 152 152 152s152-68.187 152-152s-68.187-152-152-152m0 272a120 120 0 1 1 120-120a120.136 120.136 0 0 1-120 120M240 16h32v48h-32zm0 432h32v48h-32zm208-208h48v32h-48zm-432 0h48v32H16zm372.687 171.314l22.627-22.627l32 32l-22.627 22.627zm-320-320l22.628-22.628l32 32l-22.628 22.628zm-.002 329.375l32-32l22.628 22.626l-32 32zm320.002-320.003l32-32l22.628 22.628l-32 32z' />
     </svg>
 )
 
-export const Moon = (props: any) => (
+const Moon = (props: any) => (
     <svg width='1em' height='1em' viewBox='0 0 32 32' xmlns='http://www.w3.org/2000/svg' {...props}>
         <path fill='currentColor' d='M13.503 5.414a15.076 15.076 0 0 0 11.593 18.194a11.113 11.113 0 0 1-7.975 3.39c-.138 0-.278.005-.418 0a11.094 11.094 0 0 1-3.2-21.584M14.98 3a1.002 1.002 0 0 0-.175.016a13.096 13.096 0 0 0 1.825 25.981c.164.006.328 0 .49 0a13.072 13.072 0 0 0 10.703-5.555a1.01 1.01 0 0 0-.783-1.565A13.08 13.08 0 0 1 15.89 4.38A1.015 1.015 0 0 0 14.98 3' />
     </svg>
