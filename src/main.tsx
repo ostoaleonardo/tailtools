8431ca047274c3ea26ad4117db02a2b1cc72ad2f
ostoaleonardo <ostoaleonardo.dev@gmail.com>
2024-07-01 10:31:11 -0600

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import ReactDOM from 'react-dom/client'
 import Providers from './Providers.tsx'
 import App from './App.tsx'
-import '@/styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import '@/styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <Providers>
             <App />
+            <Analytics />
         </Providers>
     </BrowserRouter>
 )
